02fcef5597eeb7a24d8fe297b33b78c03ff44be6
Blokhin Denis <sopplet@gmail.com>
2013-09-12 10:20:00 +0400

    add context variables

--- index.js
+++ index.js
@@ -6,58 +6,64 @@ var program = require('commander'),
 
 
 generator.addDirectory(path.join(process.env.HOME, 'generators'));
+generator.addContext(path.join(process.env.HOME, 'generators','default.js'));
+
 
 program.version('0.0.1');
 
 program
-  .command('list')
-  .description('run setup commands for all envs')
-  .action(function () {
-    generator.getGenerators(function (err, path) {
-      console.log(path);
-    });
+.command('list')
+.description('run setup commands for all envs')
+.action(function () {
+  generator.getGenerators(function (err, path) {
+    console.log(path);
+  });
+});
+
+program
+.command('run [name]')
+.description('run generator')
+.action(function (name) {
+
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   });
 
+  return generator.getGenerator(name, function (err, gen) {
+    if(err){
+      return console.log(err);
+    }
+    var params = gen.getParams(), iterate;
 
+    iterate = function (param, next) {
 
-  program.
-    command('run [name]').
-    description('run generator').
-    action(function (name) {
-
-    rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    return generator.getGenerator(name, function (err, gen) {
-      if(err){
-        return console.log(err);
+      if(program[param.name]){
+        gen.setParam(param.name, program[param.name]);
+        return next();
       }
-      var params = gen.getParams(), iterate;
 
-      iterate = function (param, next) {
-        rl.question(param.title+': ', function (val) {
-          var reg;
-          if(param.pattern){
-            reg = new RegExp(param.pattern);
-            if(!reg.test(val)){
-              console.log(param.title, 'must be', param.pattern);
-              return iterate(param, next);
-            }
+      return rl.question(param.title+': ', function (val) {
+        var reg;
+        if(param.pattern){
+          reg = new RegExp(param.pattern);
+          if(!reg.test(val)){
+            console.log(param.title, 'must be', param.pattern);
+            return iterate(param, next);
           }
-          gen.setParam(param.name, val);
-          return next();
-        });
-      };
+        }
+        gen.setParam(param.name, val);
+        return next();
+      });
+    };
 
-      return async.map(params, iterate, function () {
-        return gen.run(function () {
-          rl.close();
-        });
+    return async.map(params, iterate, function () {
+      return gen.run(function () {
+        rl.close();
       });
     });
   });
+});
 
 
-
-  program.parse(process.argv);
+program.parse(process.argv);
